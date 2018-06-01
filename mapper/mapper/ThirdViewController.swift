21892b79683b94900522f95e807273a8b50bcaed
Anton VYKHOVANETS <avykhova@e2r6p10.unit.ua>
2018-06-01 23:54:49 +0300

    ex02 done

---
+++ mapper/mapper/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  mapper
+//
+//  Created by Vitas BRAZAS on 6/1/18.
+//  Copyright © 2018 Vitas BRAZAS. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
