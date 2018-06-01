671143b1a5c53c6b6f76260ccd72b1607bd04189
vbrazas <appleitunesstore11@gmail.com>
2018-06-01 22:11:06 +0300

    ex00 done

---
+++ mapper/mapper/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  mapper
+//
+//  Created by Vitas BRAZAS on 6/1/18.
+//  Copyright © 2018 Vitas BRAZAS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
